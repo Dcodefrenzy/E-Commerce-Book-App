e9fde19da59571378f119de98212d7f561fde9c6
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2018-04-14 21:27:02 +0100

    Uploding User Side of ecommerce project

---
+++ www/users/catalogue.php
@@ -0,0 +1,62 @@
+<?php 
+  include "include/db.php";
+  include "include/function.php";
+  include "include/header3.php";
+
+
+  $cat_id = $_GET['cat_id'];
+
+
+
+ ?>
+
+  <!-- side bar starts here -->
+  <div class="side-bar">
+    <div class="categories">
+      <h3 class="header">Categories</h3>
+      <ul class="category-list">
+        <?php $viewAllCategories =selectCategory($conn);
+        echo $viewAllCategories; ?>
+      </ul>
+    </div>
+  </div>
+  <!-- main content starts here -->
+  <div class="main">
+    <div class="main-book-list horizontal-book-list">
+      <ul class="book-list">
+        <?php $showImageInCategory = getBookByCategory($conn, $cat_id); echo $showImageInCategory; ?>
+      </ul>
+      <div class="actions">
+        <button class="def-button previous">Previous</button>
+        <button class="def-button next">Next</button>
+      </div>
+    </div>
+    <div class="recently-viewed-books horizontal-book-list">
+      <h3 class="header">Recently Viewed</h3>
+      <ul class="book-list">
+        <div class="scroll-back"></div>
+        <div class="scroll-front"></div>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$250</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$50</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$125</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$90</p></div>
+        </li>
+      </ul>
+    </div>
+    
+    <?php 
+
+      include "include/footer.php";
+     ?>
+ 
